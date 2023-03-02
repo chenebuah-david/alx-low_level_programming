f91c13d1d5eebc4a084bceafa681adecd99b7138
chenebuah-david <davidchenebuah@gmail.com>
2023-03-01 17:12:45 -0800

    Work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-int main(void)
+int main(void);
 {
-       int n;
-       int a[5];
-       int *p;
+	int n;
+	int a[5];
+	int *p;
 
-       a[2] = 1024;
-       p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - comment
-   */
-       *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-       printf("a[2] = %d\n", a[2]);
-       return (0);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * I hope all these code is worth it
+	 * They said i need 19 lines
+	 * Let us hope this works
+	 * I have been on this particular task too long
+	 * Lord help me
+	 * only one statement
+	 */
+	*(p + 5) = 98;
+	/*Hope this is correct*/
+	printf("a[2]=%d\n", a[2]);
+	return (0);
 }
