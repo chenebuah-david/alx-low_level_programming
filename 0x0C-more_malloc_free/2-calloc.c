8634de98af6aadea325b6ba627d5fab4c548daba
chenebuah-david <davidchenebuah@gmail.com>
2023-03-16 16:47:26 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,9 +16,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
-	
+
 	{
 		s[i] = b;
 
@@ -52,7 +52,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	ptr = malloc(size * nmemb);
-	
+
 
 	if (ptr == NULL)
 		return (NULL);
@@ -60,5 +60,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
-		
+
 }
